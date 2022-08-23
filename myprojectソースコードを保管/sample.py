8ac55ec204c22a69da6de089406a3dd4255235a8
k-saito-en <saito.kotaro.engineer@gmail.com>
2022-08-23 22:42:17 +0900

    やっと簡単なツールができました！！ ヤッタァぁぁぁぁxあっぁぁ

--- myprojectソースコードを保管/sample.py
+++ myprojectソースコードを保管/sample.py
@@ -1,14 +1,29 @@
+
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import json
 
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets']
+#ServiceAccountCredentials：Googleの各サービスへアクセスできるservice変数を生成します。
+from oauth2client.service_account import ServiceAccountCredentials 
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json",scope)
+#2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
-client = gspread.authorize(creds)
+#認証情報設定
+#ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
+credentials = ServiceAccountCredentials.from_json_keyfile_name('myprojectソースコードを保管/python-spreadshee-5ae0a280e24f.json', scope)
 
-sheet = client.open("spreadsheet_2").sheet1
+#OAuth2の資格情報を使用してGoogle APIにログインします。
+gc = gspread.authorize(credentials)
 
-data =sheet.get_all_records()
+#共有設定したスプレッドシートキーを変数[SPREADSHEET_KEY]に格納する。
+SPREADSHEET_KEY = '1ODNxwa5JBDGn5z_j-fJtW68aFK75q1QjPEvcNQ4uAPs'
 
-print(data)
+#共有設定したスプレッドシートのシート1を開く
+worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
+
+#A1セルの値を受け取る
+import_value = int(worksheet.acell('A1').value)
+
+#A1セルの値に100加算した値をB1セルに表示させる
+export_value = import_value+100
+worksheet.update_cell(1,2, export_value)
