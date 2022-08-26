6f3b0501e0b24e5c5853e70e241628389a8ee334
齊藤虎太郎 <111550856+k-saito-en@users.noreply.github.com>
2022-08-26 12:22:29 +0900

    リポジトリの作成

---
+++ spreadsheet_test_2/myprojectソースコードを保管/sample.py
@@ -0,0 +1,29 @@
+
+import gspread
+import json
+
+#ServiceAccountCredentials：Googleの各サービスへアクセスできるservice変数を生成します。
+from oauth2client.service_account import ServiceAccountCredentials 
+
+#2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+#認証情報設定
+#ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
+credentials = ServiceAccountCredentials.from_json_keyfile_name('myprojectソースコードを保管/python-spreadshee-5ae0a280e24f.json', scope)
+
+#OAuth2の資格情報を使用してGoogle APIにログインします。
+gc = gspread.authorize(credentials)
+
+#共有設定したスプレッドシートキーを変数[SPREADSHEET_KEY]に格納する。
+SPREADSHEET_KEY = '1ODNxwa5JBDGn5z_j-fJtW68aFK75q1QjPEvcNQ4uAPs'
+
+#共有設定したスプレッドシートのシート1を開く
+worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
+
+#A1セルの値を受け取る
+import_value = int(worksheet.acell('A1').value)
+
+#A1セルの値に100加算した値をB1セルに表示させる
+export_value = import_value+100
+worksheet.update_cell(1,2, export_value)
